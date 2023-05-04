a9b3db9fb06af9eb51fd5ca191b26740a6082861
Mahendrata Harpi <id.harpi@proton.me>
2023-05-04 20:15:16 +0700

    refactor: change class name to AddThread

--- src/Domains/threads/entities/AddThread.js
+++ src/Domains/threads/entities/AddThread.js
@@ -1,6 +1,4 @@
-const {type} = require("@hapi/hapi/lib/headers");
-
-class PostThread {
+class AddThread {
   constructor(payload) {
     this.verifyPayload(payload);
 
@@ -12,13 +10,13 @@ class PostThread {
     const { title, body } = payload
 
     if(!title || !body) {
-      throw new Error('POST_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')
+      throw new Error('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')
     }
 
     if(typeof title !== 'string' || typeof body !== 'string') {
-      throw new Error('POST_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')
+      throw new Error('ADD_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')
     }
   }
 }
 
-module.exports = PostThread
+module.exports = AddThread
