0e9a5e6c9ba6507ae7b268f009dc8984a9deaf54
Mahendrata Harpi <id.harpi@proton.me>
2023-05-04 19:26:49 +0700

    feat: tread repository interface

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,7 @@
+class ThreadRepository {
+  async addThread(postThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+}
+
+module.exports = ThreadRepository
---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,11 @@
+const ThreadRepository = require('../ThreadRepository')
+
+describe('ThreadRepository Interface', () => {
+  it('should throw error when invoke abstract behavior', () =>  {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+
+    // Action & Assert
+    expect(threadRepository.addThread({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+})
