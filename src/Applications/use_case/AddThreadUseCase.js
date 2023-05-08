76bdf19f80910c9ae66283c3c790bfa974878b18
Mahendrata Harpi <id.harpi@proton.me>
2023-05-08 14:52:31 +0700

    feat: add thread use case

---
+++ src/Applications/use_case/AddThreadUseCase.js
@@ -0,0 +1,16 @@
+const AddThread = require('../../Domains/threads/entities/AddThread')
+
+class AddThreadUseCase {
+  constructor({ threadRepository }) {
+    this._threadRepository = threadRepository
+  }
+
+  async execute(useCasePayload) {
+    const addThread = new AddThread(useCasePayload)
+
+    await this._threadRepository.verifyAvailableTitle(useCasePayload.title)
+    return this._threadRepository.addThread(addThread);
+  }
+}
+
+module.exports = AddThreadUseCase
