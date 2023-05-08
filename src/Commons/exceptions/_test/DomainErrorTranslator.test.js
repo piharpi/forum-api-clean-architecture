093677461c2ea18da0024c77fa04664e3083689c
Mahendrata Harpi <id.harpi@proton.me>
2023-05-08 14:55:39 +0700

    test: add domain error test case

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -11,11 +11,12 @@ describe('DomainErrorTranslator', () => {
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'));
-    expect(DomainErrorTranslator.translate(new Error('POST_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')))
       .toStrictEqual(new InvariantError('harus mengirimkan title dan body'));
-    expect(DomainErrorTranslator.translate(new Error('POST_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('ADD_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('title dan body harus string'));
-
+    expect(DomainErrorTranslator.translate(new Error('ADD_THREAD.TITTLE_LIMIT_CHAR')))
+      .toStrictEqual(new InvariantError('title melebihi batas yang ditentukan'));
   });
 
   it('should return original error when error message is not needed to translate', () => {
