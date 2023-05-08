d0e6d44e02f65b78abd31f6cac49dfae50ad5128
Mahendrata Harpi <id.harpi@proton.me>
2023-05-08 14:59:59 +0700

    feat: thread repository test case

--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -8,4 +8,12 @@ describe('ThreadRepository Interface', () => {
     // Action & Assert
     expect(threadRepository.addThread({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
+
+  it('should throw error when invoke abstract behavior', () =>  {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+
+    // Action & Assert
+    expect(threadRepository.verifyAvailableTitle('')).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
 })
