8750d8c8d69bf258fe377ee25d40ff20b889b30e
Mahendrata Harpi <id.harpi@proton.me>
2023-05-10 02:40:13 +0700

    feat: add test comment table helper

---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const CommentsTableTestHelper = {
+  async addComment(
+    {
+      id = 'comment-123',
+      content = 'ini komentar',
+      thread = 'thread-123',
+      owner = 'user-123',
+      is_delete = false
+    }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5)',
+      values: [id, content, thread, owner, is_delete],
+    };
+
+    await pool.query(query);
+  },
+
+  async findCommentsById(id) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id],
+    };
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM comments WHERE 1=1');
+  },
+};
+
+module.exports = CommentsTableTestHelper;
