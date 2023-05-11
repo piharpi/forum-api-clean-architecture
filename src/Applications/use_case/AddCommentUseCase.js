bc82f7d9cdc6bdd32fa6c1e11b0432cd6ed8ffc1
Mahendrata Harpi <id.harpi@proton.me>
2023-05-11 10:47:12 +0700

    feat: add thread validation

--- src/Applications/use_case/AddCommentUseCase.js
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -1,10 +1,13 @@
 const NewComment = require('../../Domains/comments/entities/NewComment');
 
 class AddCommentUseCase {
-  constructor({ commentRepository }) {
+  constructor({ commentRepository, threadRepository }) {
     this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository
   }
   async execute(useCasePayload) {
+
+    await this._threadRepository.checkIsThreadExist(useCasePayload.thread);
     const newComment = new NewComment(useCasePayload)
 
     return this._commentRepository.addComment(newComment);
