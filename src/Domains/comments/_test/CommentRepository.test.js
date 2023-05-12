89fcfdc68642e31a9a808037aaf770e731493247
Mahendrata Harpi <id.harpi@proton.me>
2023-05-12 11:01:59 +0700

    test: create test case

--- src/Domains/comments/_test/CommentRepository.test.js
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -8,4 +8,36 @@ describe('CommentRepository Interface', () => {
     // Action & Assert
     expect(commentRepository.addComment({})).rejects.toThrow('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
+
+  it('should throw error when invoke abstract behavior', () =>  {
+    // Arrange
+    const commentRepository = new CommentRepository();
+
+    // Action & Assert
+    expect(commentRepository.checkIsCommentAvailable('')).rejects.toThrow('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+
+  it('should throw error when invoke abstract behavior', () =>  {
+    // Arrange
+    const commentRepository = new CommentRepository();
+
+    // Action & Assert
+    expect(commentRepository.findCommentById('')).rejects.toThrow('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+
+  it('should throw error when invoke abstract behavior', () =>  {
+    // Arrange
+    const commentRepository = new CommentRepository();
+
+    // Action & Assert
+    expect(commentRepository.deleteComment('')).rejects.toThrow('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+
+  it('should throw error when invoke abstract behavior', () =>  {
+    // Arrange
+    const commentRepository = new CommentRepository();
+
+    // Action & Assert
+    expect(commentRepository.verifyCommentOwner('')).rejects.toThrow('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
 })
