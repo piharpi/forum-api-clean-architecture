89ad241e42cb301e5292b8052f6323603327389a
Mahendrata Harpi <id.harpi@proton.me>
2023-05-12 11:05:20 +0700

    feat: deleteCommentHandler

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,10 +1,12 @@
-const CommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase')
+const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase')
+const DeleteCommentUseCase = require('../../../../Applications/use_case/DeleteCommentUseCase')
 
 class CommentsHandler {
   constructor(container) {
     this._container = container;
 
     this.postCommentHandler = this.postCommentHandler.bind(this);
+    this.deleteCommentHandler = this.deleteCommentHandler.bind(this);
   }
 
   async postCommentHandler(request, h) {
@@ -13,7 +15,7 @@ class CommentsHandler {
 
     const useCasePayload = { owner, thread, ...request.payload };
 
-    const commentUseCase = this._container.getInstance(CommentUseCase.name);
+    const commentUseCase = this._container.getInstance(AddCommentUseCase.name);
     const addedComment = await commentUseCase.execute(useCasePayload);
 
     const response = h.response({
@@ -24,6 +26,22 @@ class CommentsHandler {
     });
     response.code(201);
     return response
+  }
+
+  async deleteCommentHandler(request, h) {
+    const ownerId = request.auth.credentials.id;
+    const { threadId, commentId } = request.params;
+    const useCasePayload = { ownerId, threadId, commentId }
+
+    const deleteCommentUseCase = this._container.getInstance(DeleteCommentUseCase.name);
+    await deleteCommentUseCase.execute(useCasePayload);
+
+    const response = h.response({
+      status: 'success'
+    });
+
+    response.code(200);
+    return response;
   }
 }
 
