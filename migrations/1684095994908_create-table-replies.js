fdec986150d9c0893f2856c8e678e495cd912e3d
Mahendrata Harpi <id.harpi@proton.me>
2023-05-15 04:06:54 +0700

    feat: create new table replies

---
+++ migrations/1684095994908_create-table-replies.js
@@ -0,0 +1,60 @@
+/* eslint-disable camelcase */
+exports.up = pgm => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    thread: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    comment: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    }
+  });
+
+  pgm.addConstraint(
+    "replies",
+    "fk_replies.owner_users.id",
+    "FOREIGN KEY(owner) REFERENCES users(id) ON DELETE NO ACTION ON UPDATE CASCADE"
+  );
+
+  pgm.addConstraint(
+    "replies",
+    "fk_replies.thread_threads.id",
+    "FOREIGN KEY(thread) REFERENCES threads(id) ON DELETE SET NULL ON UPDATE CASCADE"
+  );
+
+  pgm.addConstraint(
+    "replies",
+    "fk_replies.comment_comments.id",
+    "FOREIGN KEY(comment) REFERENCES comments(id) ON DELETE SET NULL ON UPDATE CASCADE"
+  );
+};
+
+exports.down = pgm => {
+  pgm.dropConstraint("replies", "fk_replies.thread_threads.id");
+  pgm.dropConstraint("replies", "fk_replies.owner_users.id");
+  pgm.dropConstraint("replies", "fk_replies.comment_comments.id");
+  pgm.dropTable('replies')
+};
