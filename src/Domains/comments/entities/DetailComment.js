b9e05d8f750cb86c70d83f0351f4117dd9f715d1
Mahendrata Harpi <id.harpi@proton.me>
2023-05-15 12:41:15 +0700

    feat: add replies property

--- src/Domains/comments/entities/DetailComment.js
+++ src/Domains/comments/entities/DetailComment.js
@@ -1,16 +1,17 @@
 class DetailComment {
   constructor(payload) {
-    const { username, content, date, id , is_delete } = payload;
+    const { username, content, date, id, replies, is_delete } = payload;
 
     this._verifyPayload(payload);
 
     this.id = id;
     this.username = username;
     this.content = is_delete ? '**komentar telah dihapus**' : content;
+    this.replies = replies || [];
     this.date = date;
   }
 
-  _verifyPayload({ username, content, date, id, is_delete }) {
+  _verifyPayload({ username, content, date, id, is_delete, replies = []}) {
     if(!id || !content || !username || !date) {
       throw new Error('DETAIL_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY')
     }
@@ -19,7 +20,8 @@ class DetailComment {
       || typeof content !== 'string'
       || typeof username !== 'string'
       || typeof date !== 'string'
-      || typeof is_delete !== 'boolean'){
+      || typeof is_delete !== 'boolean'
+      || Array.isArray(replies) ){
       throw new Error('DETAIL_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION')
     }
   }
