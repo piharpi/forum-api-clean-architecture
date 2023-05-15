ef9b363a630e08c02ceda19c837feca425f58967
Mahendrata Harpi <id.harpi@proton.me>
2023-05-15 14:40:06 +0700

    feat: add reply use case complete

---
+++ src/Applications/use_case/AddReplyUseCase.js
@@ -0,0 +1,19 @@
+const NewReply = require('../../Domains/replies/entities/NewReply');
+
+class AddReplyUseCase {
+  constructor({ commentRepository, threadRepository, replyRepository }) {
+    this._replyRepository = replyRepository;
+    this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository
+  }
+  async execute(useCasePayload) {
+
+    await this._threadRepository.checkIsThreadAvailable(useCasePayload.thread);
+    await this._commentRepository.checkIsCommentAvailable(useCasePayload.comment);
+    const newReply = new NewReply(useCasePayload)
+
+    return this._replyRepository.addReply(newReply);
+  }
+}
+
+module.exports = AddReplyUseCase;
