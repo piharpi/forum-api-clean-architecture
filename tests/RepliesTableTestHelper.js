9a4fdf0d66ec0d13367c2e8add49ae09487b242b
Mahendrata Harpi <id.harpi@proton.me>
2023-05-15 12:45:01 +0700

    feat: add replies test helper

---
+++ tests/RepliesTableTestHelper.js
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const RepliesTableTestHelper = {
+  async addReply(
+    {
+      id = 'reply-123',
+      content = 'ini balasan',
+      thread = 'thread-123',
+      owner = 'user-123',
+      comment = 'comment-123',
+      is_delete = false
+    }) {
+    const query = {
+      text: 'INSERT INTO replies(id, content, owner, thread, comment, is_delete) VALUES($1, $2, $3, $4, $5, $6);',
+      values: [id, content, owner, thread, comment, is_delete],
+    };
+
+    await pool.query(query);
+  },
+
+  async findRepliesById(id) {
+    const query = {
+      text: 'SELECT * FROM replies WHERE id = $1',
+      values: [id],
+    };
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM replies WHERE 1=1');
+  },
+};
+
+module.exports = RepliesTableTestHelper;
