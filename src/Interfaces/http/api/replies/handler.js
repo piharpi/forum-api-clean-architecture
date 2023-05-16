11aaf72d7ae235ad34315d354e473dc6b0cad446
Mahendrata Harpi <id.harpi@proton.me>
2023-05-16 11:17:18 +0700

    feat: delete route and handler

--- src/Interfaces/http/api/replies/handler.js
+++ src/Interfaces/http/api/replies/handler.js
@@ -1,12 +1,12 @@
 const AddReplyUseCase = require('../../../../Applications/use_case/AddReplyUseCase')
-// const DeleteReplyUseCase = require('../../../../Applications/use_case/DeleteReplyUseCase')
+const DeleteReplyUseCase = require('../../../../Applications/use_case/DeleteReplyUseCase')
 
 class RepliesHandler {
   constructor(container) {
     this._container = container;
 
     this.postReplyHandler = this.postReplyHandler.bind(this);
-    // this.deleteReplyHandler = this.deleteReplyHandler.bind(this);
+    this.deleteReplyHandler = this.deleteReplyHandler.bind(this);
   }
 
   async postReplyHandler(request, h) {
@@ -29,21 +29,21 @@ class RepliesHandler {
     return response
   }
 
-  // async deleteReplyHandler(request, h) {
-  //   const ownerId = request.auth.credentials.id;
-  //   const { threadId, commentId } = request.params;
-  //   const useCasePayload = { ownerId, threadId, commentId }
-  //
-  //   const deleteReplyUseCase = this._container.getInstance(DeleteReplyUseCase.name);
-  //   await deleteReplyUseCase.execute(useCasePayload);
-  //
-  //   const response = h.response({
-  //     status: 'success'
-  //   });
-  //
-  //   response.code(200);
-  //   return response;
-  // }
+  async deleteReplyHandler(request, h) {
+    const ownerId = request.auth.credentials.id;
+    const { threadId, commentId, replyId } = request.params;
+    const useCasePayload = { ownerId, threadId, commentId, replyId }
+
+    const deleteReplyUseCase = this._container.getInstance(DeleteReplyUseCase.name);
+    await deleteReplyUseCase.execute(useCasePayload);
+
+    const response = h.response({
+      status: 'success'
+    });
+
+    response.code(200);
+    return response;
+  }
 }
 
 module.exports = RepliesHandler
