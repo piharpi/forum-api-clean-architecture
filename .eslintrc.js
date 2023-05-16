4f7645b42e8cf2b7d93b065c6b0bdaa5233c5873
Mahendrata Harpi <id.harpi@proton.me>
2023-05-16 14:23:33 +0700

    style: airbnb style guides

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  plugins: ["prettier"],
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+    "class-methods-use-this": "off",
+    "no-underscore-dangle": "off",
+  },
+};
--- src/Applications/use_case/DetailThreadUseCase.js
+++ src/Applications/use_case/DetailThreadUseCase.js
@@ -9,23 +9,42 @@ class DetailThreadUseCase {
     await this._threadRepository.checkIsThreadAvailable(
       useCasePayload.threadId
     );
+
     const detailThread = await this._threadRepository.getDetailThreadById(
       useCasePayload.threadId
     );
 
-    detailThread.comments =
+    const comments =
       await this._commentRepository.getAllDetailCommentByThreadId(
         useCasePayload.threadId
       );
 
-    for (const comment of detailThread.comments) {
-      comment.replies =
-        await this._replyRepository.getAllDetailReplyByThreadAndCommentId(
-          useCasePayload.threadId,
-          comment.id
-        );
-    }
+    const requestReply = async (threadId, commentId) =>
+      this._replyRepository.getAllDetailReplyByThreadAndCommentId(
+        threadId,
+        commentId
+      );
 
+    const replies = comments.map((comment) =>
+      requestReply(useCasePayload.threadId, comment.id)
+    );
+
+    const solvedPromisesReplies = await Promise.all(replies);
+
+    solvedPromisesReplies.forEach((r, i) => {
+      comments[i].replies = r;
+    });
+
+    // another solution but not recommended
+    // for (const comment of detailThread.comments) {
+    //   comment.replies =
+    //     await this._replyRepository.getAllDetailReplyByThreadAndCommentId(
+    //       useCasePayload.threadId,
+    //       comment.id
+    //     );
+    // }
+
+    detailThread.comments = comments;
     return detailThread;
   }
 }
